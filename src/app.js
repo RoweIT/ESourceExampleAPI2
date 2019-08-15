1b49dfd0fbe1862445d3199ed1fae7f886201813
Adam Nicholas <adam.nicholas@roweit.co.uk>
2019-08-15 13:19:54 +0100

    Testing deployment

--- src/app.js
+++ src/app.js
@@ -1,40 +1,33 @@
 let users = [
-  'Second project!',
   {
-    'id': 1,
-    'Name': 'Adam',
-    'Admin': true
+    id: 1,
+    Name: 'Adam',
+    Admin: true
   },
   {
-    'id': 2,
-    'Name': 'Paul',
-    'Admin': true
+    id: 2,
+    Name: 'Paul',
+    Admin: true
   },
   {
-    'id': 3,
-    'Name': 'Steve',
-    'Admin': true
+    id: 3,
+    Name: 'Steve',
+    Admin: true
   },
   {
-    'id': 4,
-    'Name': 'Gary',
-    'Admin': true
+    id: 4,
+    Name: 'Gary',
+    Admin: true
   },
   {
-    'id': 5,
-    'Name': 'Test User'
+    id: 5,
+    Name: 'Tim',
+    Admin: true
   },
   {
-    'id': 6,
-    'Name': 'Test User 2'
-  },
-  {
-    'id': 7,
-    'Name': 'Test User 3'
-  },
-  {
-    'id': 8,
-    'Name': 'Test User 4'
+    id: 99,
+    Name: 'Joe',
+    admin: false
   }
 ];
 /**
@@ -42,34 +35,90 @@ let users = [
  * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
  * @param {Object} event - API Gateway Lambda Proxy Input Format
  *
- * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html 
+ * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html
  * @param {Object} context
  *
  * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
  * @returns {Object} object - API Gateway Lambda Proxy Output Format
- * 
+ *
  */
-exports.lambdaHandler = async (event, context) => {
+
+let responseBody = {
+  apiVersion: 2
+};
+
+exports.getAllUsers = async (event, context) => {
   try {
     console.log('Received event:', JSON.stringify(event, null, 2));
-    let responseBody = '';
+    responseBody.users = getAllUsers();
 
-    if (event.pathParameters && event.pathParameters.id) {
-      responseBody = getUser(parseInt(event.pathParameters.id));
-    } else {
-      responseBody = getAllUsers();
-    }
-
-    return response = {
+    return {
       statusCode: 200,
       body: JSON.stringify(responseBody),
       headers: {
         'Access-Control-Allow-Origin': '*'
       }
-    }
+    };
   } catch (err) {
-    console.log(err);
-    return err;
+    return {
+      statusCode: 401,
+      body: {
+        message: err.message
+      },
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      }
+    };
+  }
+};
+
+exports.getUser = async (event, context) => {
+  try {
+    console.log('Received event:', JSON.stringify(event, null, 2));
+    responseBody.user = getUser(parseInt(event.pathParameters.id));
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(responseBody),
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      }
+    };
+  } catch (err) {
+    return {
+      statusCode: 401,
+      body: {
+        message: err.message
+      },
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      }
+    };
+  }
+};
+
+exports.createUser = async (event, context) => {
+  try {
+    console.log('Received event:', JSON.stringify(event, null, 2));
+    let user = event.body;
+
+    return {
+      statusCode: 201,
+      body: JSON.stringify(user),
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      }
+    };
+  } catch (err) {
+    return {
+      statusCode: 401,
+      body: {
+        message: err.message
+      },
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      }
+    };
   }
 };
 
@@ -77,12 +126,12 @@ const getAllUsers = () => {
   return users;
 };
 
-const getUser = (id) => {
+const getUser = id => {
   let filteredUsers = users.filter(user => user.id === id);
 
-  if (filteredUsers.length > 0) return filteredUsers
+  if (filteredUsers.length > 0) return filteredUsers;
 
   return {
-    'message': 'User not found!'
+    message: 'User not found!'
   };
 };
