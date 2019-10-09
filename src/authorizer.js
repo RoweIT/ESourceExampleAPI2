ff08fcc9bd4fdba91609dfccbdfc9eca307b242b
Adam Nicholas <adam.nicholas@roweit.co.uk>
2019-10-09 11:03:24 +0100

    Updated authorizer to use cognito user pool

--- src/authorizer.js
+++ src/authorizer.js
@@ -1,4 +1,7 @@
+const AWS = require('aws-sdk');
+
 /**
+ *
  *
  * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
  * @param {Object} event - API Gateway Lambda Proxy Input Format
@@ -10,33 +13,55 @@
  * @returns {Object} object - API Gateway Lambda Proxy Output Format
  *
  */
-exports.handler = async (event, context) => {
+exports.handler = async (event, context, callback) => {
   try {
-    let token = event.authorizationToken;
-    console.log(`Token: ${token}`);
-    console.log(`Event: ${event}`);
+    const cognitoidentityserviceprovider = new AWS.CognitoIdentityServiceProvider(
+      {
+        region: 'eu-west-2'
+      }
+    );
 
-    if (token) return generatePolicy(1, 'Allow', event.methodArn);
-    else return generatePolicy(1, 'Deny', event.methodArn);
-  } catch (err) {
-    const response = {
-      statusCode: 401,
-      headers: {
-        'Access-Control-Allow-Origin': '*'
-      },
-      body: JSON.stringify({
-        error: e.message
-      })
+    console.log(`Event: ${JSON.stringify(event)}`);
+    console.log(`Env: ${JSON.stringify(process.env)}`);
+
+    const authHeader = event.authorizationToken;
+    const { COGNITO_CLIENT_ID } = process.env;
+
+    console.log(`Authorization header: ${authHeader}`);
+
+    if (!authHeader) return callback('Unauthorized');
+
+    const plainCreds = Buffer.from(authHeader.split(' ')[1], 'base64')
+      .toString()
+      .split(':');
+
+    const params = {
+      AuthFlow: 'USER_PASSWORD_AUTH',
+      ClientId: COGNITO_CLIENT_ID,
+      AuthParameters: {
+        USERNAME: plainCreds[0],
+        PASSWORD: plainCreds[1]
+      }
     };
 
-    callback(null, response);
+    return cognitoidentityserviceprovider
+      .initiateAuth(params, () => {})
+      .promise()
+      .then(res => {
+        console.log(JSON.stringify(res));
+        return generatePolicy(plainCreds[0], 'Allow', event.methodArn);
+      })
+      .catch(err => {
+        console.error(JSON.stringify(err));
+        return generatePolicy(plainCreds[0], 'Deny', event.methodArn);
+      });
+  } catch (err) {
+    callback(Error(err));
   }
-
-  return response;
 };
 
 // Help function to generate an IAM policy
-const generatePolicy = async (principalId, effect, methodArn) => {
+function generatePolicy(principalId, effect, methodArn) {
   let authResponse = {};
 
   authResponse.principalId = principalId;
@@ -56,4 +81,4 @@ const generatePolicy = async (principalId, effect, methodArn) => {
   }
 
   return authResponse;
-};
+}
